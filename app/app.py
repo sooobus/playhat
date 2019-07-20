c487d39da7e093161b69ac331211d859f69e63eb
sooobus <sooobus@gmail.com>
2019-07-20 13:59:17 +0300

    Add flask support and remove examples

---
+++ app/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+# Set up custom delimiters to avoid Vue.js conflict.
+
+class CustomFlask(Flask):
+  jinja_options = Flask.jinja_options.copy()
+  jinja_options.update(dict(
+    block_start_string='(%',
+    block_end_string='%)',
+    variable_start_string='((',
+    variable_end_string='))',
+    comment_start_string='(#',
+    comment_end_string='#)',
+  ))
+
+app = CustomFlask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
